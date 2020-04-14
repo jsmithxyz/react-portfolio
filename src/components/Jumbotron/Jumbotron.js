367784192694a2f561e9f262bf39c6f789c16fd1
jsmithxyz <jordansmith3813@gmail.com>
2020-04-14 15:56:08 -0400

    added styling to jumbotron

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Jumbotron, Container } from "react-bootstrap";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
+import "./jumbotron.css";
 
 class JumbotronComp extends React.Component {
   render() {
     return (
-      <Jumbotron fluid>
+      <Jumbotron className='jumbo' fluid>
         <Container>
-          <h1>Jordan Smith</h1>
-          <p>.web developer.</p>
-          <Fab color='primary' aria-label='add'>
+          <h1 className='nameLead'>Jordan Smith</h1>
+          <p className='titleLead'>.web developer.</p>
+          <Fab className='jumboFab' aria-label='add'>
             <AddIcon />
           </Fab>
         </Container>
